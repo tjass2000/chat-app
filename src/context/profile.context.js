cc2c391e3e0c43d48795d1187693bfc19a0c1fbd
tjass2000 <tejaskhera191812@gmail.com>
2021-06-23 20:02:57 +0530

    manage user with profile context and put real time subs

---
+++ src/context/profile.context.js
@@ -0,0 +1,51 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { auth, database } from '../misc/firebase';
+
+const ProfileContext = createContext();
+
+export const ProfileProvider = ({ children }) => {
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    let userRef;
+    const authUnsub = auth.onAuthStateChanged(authObj => {
+      if (authObj) {
+        userRef = database.ref(`/profiles/${authObj.uid}`);
+        userRef.on('value', snap => {
+          const { name, createdAt } = snap.val();
+
+          const data = {
+            name,
+            createdAt,
+            uid: authObj.uid,
+            email: authObj.email,
+          };
+          setProfile(data);
+          setisLoading(false);
+        });
+      } else {
+        if (userRef) {
+          userRef.off();
+        }
+        setProfile(null);
+        setisLoading(false);
+      }
+    });
+    return () => {
+      authUnsub();
+
+      if (userRef) {
+        userRef.off();
+      }
+    };
+  }, []);
+
+  return (
+    <ProfileContext.Provider value={{ isLoading, profile }}>
+      {children}
+    </ProfileContext.Provider>
+  );
+};
+
+export const useProfile = () => useContext(ProfileContext);

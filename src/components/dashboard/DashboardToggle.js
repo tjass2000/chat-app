888ad288eb593497c9b76c0de9e82b4bc4ba7861
tjass2000 <tejaskhera191812@gmail.com>
2021-06-28 23:13:32 +0530

    added realtime presence listener for a user

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,8 +1,9 @@
 import React, { useCallback } from 'react';
 import { Alert, Button, Drawer, Icon } from 'rsuite';
 import Dashboard from '.';
+import { isOfflineForDatabase } from '../../context/profile.context';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
-import { auth } from '../../misc/firebase';
+import { auth, database } from '../../misc/firebase';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModalState();
@@ -10,9 +11,16 @@ const DashboardToggle = () => {
   const isMobile = useMediaQuery('max-width: 992px');
 
   const onSignOut = useCallback(() => {
-    auth.signOut();
-    Alert.info('Signed out', 4000);
-    close();
+    database
+      .ref(`/status/${auth.currentUser.uid}`)
+      .set(isOfflineForDatabase)
+      .then(() => {
+        auth.signOut();
+        close();
+      })
+      .catch(err => {
+        Alert.error(err.message, 4000);
+      });
   }, [close]);
 
   return (

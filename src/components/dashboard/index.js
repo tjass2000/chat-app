21008b24baa65d7a76e9baa5b9a9ed18388aafc9
tjass2000 <tejaskhera191812@gmail.com>
2021-06-28 11:56:26 +0530

    update all user references if data is updated

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Drawer, Button, Divider, Alert } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import { database } from '../../misc/firebase';
+import { getUserUpdates } from '../../misc/helpers';
 import { AvatarUploadBtn } from './AvatarUploadBtn';
 import { EditableInput } from './EditableInput';
 import { ProviderBlock } from './ProviderBlock';
@@ -10,12 +11,15 @@ const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
   const onSave = async newData => {
-    const userNicknameRef = database
-      .ref(`/profiles/${profile.uid}`)
-      .child('name');
-
     try {
-      await userNicknameRef.set(newData);
+      const updates = await getUserUpdates(
+        profile.uid,
+        'name',
+        newData,
+        database
+      );
+
+      await database.ref().update(updates);
 
       Alert.success('Nickname has been updated', 4000);
     } catch (err) {

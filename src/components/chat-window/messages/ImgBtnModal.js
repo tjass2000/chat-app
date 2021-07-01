a0273d109295306f841dab6fac84cbda781032da
tjass2000 <tejaskhera191812@gmail.com>
2021-07-01 16:19:14 +0530

    displayed uploaded files

---
+++ src/components/chat-window/messages/ImgBtnModal.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Modal } from 'rsuite';
+import { useModalState } from '../../../misc/custom-hooks';
+
+export const ImgBtnModal = ({ src, fileName }) => {
+  const { isOpen, open, close } = useModalState();
+  return (
+    <>
+      <input
+        type="image"
+        src={src}
+        alt="file"
+        onClick={open}
+        className="mw-100 mh-100 w-auto"
+      />
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>{fileName}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <img src={src} height="100%" width="100%" alt="file" />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <a href={src} target="blank" rel="noopener noreferrer">
+            View original
+          </a>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
